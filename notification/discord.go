234d54dd3da323d0ae2636de2d8ddb315c48f5d7
pentakihyun <71832733+kihyun1998@users.noreply.github.com>
2024-10-21 16:30:52 +0900

    Add: add notification code

---
+++ notification/discord.go
@@ -0,0 +1,63 @@
+package notification
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var (
+	httpClient = &http.Client{Timeout: 10 * time.Second}
+)
+
+type DiscordMessage struct {
+	Content string  `json:"content"`
+	Embeds  []Embed `json:"embeds"`
+}
+
+type Embed struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Color       int    `json:"color"`
+}
+
+func SendDiscordAlert(embed Embed, discordWebhookURL string) error {
+	message := DiscordMessage{
+		Embeds: []Embed{embed},
+	}
+	jsonPayload, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", discordWebhookURL, bytes.NewBuffer(jsonPayload))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusNoContent {
+		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	return nil
+}
+
+func GetColorForDiscord(signal string) int {
+	switch signal {
+	case "LONG":
+		return 0x00FF00 // Green
+	case "SHORT":
+		return 0xFF0000 // Red
+	default:
+		return 0x0000FF // Blue
+	}
+}
