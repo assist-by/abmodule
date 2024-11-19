594ccb1ef0d8a4525f5ac2fc2944d5a28751b207
박기현 <kihyun1998@penta.co.kr>
2024-11-19 21:16:50 +0900

    Update: rollback ema200

--- calculate/calculate_ema.go
+++ calculate/calculate_ema.go
@@ -1,30 +1,10 @@
 package calculate
 
 // EMA 계산 float 반환
-// func CalculateEMA(prices []float64, period int) float64 {
-// 	k := 2.0 / float64(period+1)
-// 	ema := prices[0]
-// 	for i := 1; i < len(prices); i++ {
-// 		ema = prices[i]*k + ema*(1-k)
-// 	}
-// 	return ema
-// }
 func CalculateEMA(prices []float64, period int) float64 {
-	if len(prices) < period {
-		return 0
-	}
-
-	// 첫 period 동안의 SMA 계산
-	sma := 0.0
-	for i := 0; i < period; i++ {
-		sma += prices[i]
-	}
-	sma = sma / float64(period)
-
-	// EMA 계산
 	k := 2.0 / float64(period+1)
-	ema := sma
-	for i := period; i < len(prices); i++ {
+	ema := prices[0]
+	for i := 1; i < len(prices); i++ {
 		ema = prices[i]*k + ema*(1-k)
 	}
 	return ema
